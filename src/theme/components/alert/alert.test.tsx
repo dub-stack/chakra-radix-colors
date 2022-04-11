cfb90664c9358994ab7a7010d3b230108823fcb1
Spencer Duball <spencer@spencerduball.com>
2022-04-11 14:45:03 -0500

    test(accordion): Added test for the hover style that was updated.

--- src/theme/components/alert/alert.test.tsx
+++ src/theme/components/alert/alert.test.tsx
@@ -5,6 +5,7 @@ import {
   AlertIcon,
   AlertTitle,
   AlertDescription,
+  chakra,
 } from "@chakra-ui/react";
 
 describe("`solid` variant", () => {
@@ -93,4 +94,63 @@ describe("`solid` variant", () => {
     expect(alert).toHaveStyleRule("background", "var(--chakra-colors-red-9)");
     expect(alert).toHaveStyleRule("color", "var(--chakra-colors-_gray-1)");
   });
+});
+
+describe("`subtle` variant", () => {
+  test("renders correctly with 'Light' color schemes", () => {
+    const Scene = () => {
+      return (
+        <Alert
+          status="success"
+          mx="auto"
+          alignItems="start"
+          data-testid="alert"
+        >
+          <AlertIcon data-testid="alert-icon" />
+          <chakra.div flex="1">
+            <AlertTitle>Holy Smokes</AlertTitle>
+            <AlertDescription>Something just happened!</AlertDescription>
+          </chakra.div>
+        </Alert>
+      );
+    };
+    render(<Scene />);
+
+    // get dom elements
+    const alert = screen.getByTestId("alert");
+    const alertIcon = screen.getByTestId("alert-icon");
+    const toggleColorMode = screen.getByTestId("toggle-color-mode");
+    const resetColorMode = screen.getByTestId("reset-color-mode");
+
+    // reset the color mode
+    resetColorMode.click();
+
+    // test when light mode
+    expect(alert).toHaveStyleRule("background", "var(--chakra-colors-green-4)");
+    expect(alertIcon).toHaveStyleRule("color", "var(--chakra-colors-green-9)");
+
+    // switch to dark mode
+    toggleColorMode.click();
+
+    // test when dark mode
+    expect(alert).toHaveStyleRule(
+      "background",
+      "var(--chakra-colors-greenDark-4)"
+    );
+    expect(alertIcon).toHaveStyleRule(
+      "color",
+      "var(--chakra-colors-greenDark-9)"
+    );
+  });
+  test("renders correctly with 'Dark' color schemes", () => {});
+});
+
+describe.skip("`left-accent` variant", () => {
+  test("renders correctly with 'Light' color schemes", () => {});
+  test("renders correctly with 'Dark' color schemes", () => {});
+});
+
+describe.skip("`top-accent` variant", () => {
+  test("renders correctly with 'Light' color schemes", () => {});
+  test("renders correctly with 'Dark' color schemes", () => {});
 });
