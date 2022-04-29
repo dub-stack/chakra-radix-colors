f61159d0dfea1b8806af18fec5fa33aac25e8f43
Spencer Duball <spencer@spencerduball.com>
2022-04-29 14:24:21 -0500

    test(checkbox): Refactored checkbox after fixing issue from input

--- cypress/integration/components/checkbox.spec.ts
+++ cypress/integration/components/checkbox.spec.ts
@@ -8,12 +8,11 @@ describe("Default", () => {
     cy.get("#reset-color-mode").realHover().click(); // Important! Need to .realHover() to remove the hover state from prior tests.
   });
 
-  it("displays dark color in dark mode correctly", () => {
+  it("displays light color in light mode correctly", () => {
     const getCheckboxes = () =>
       cy.get(
-        "[data-testid=NewDefault] [data-testid=dark-checkboxes] .chakra-checkbox__control"
+        "[data-testid=NewDefault] [data-testid=light-checkboxes] .chakra-checkbox__control"
       );
-    cy.get("#switch-color-mode").click();
 
     // test the checked state
     const checked = getCheckboxes().eq(0);
@@ -187,11 +186,12 @@ describe("Default", () => {
     );
   });
 
-  it("displays light color in light mode correctly", () => {
+  it("displays dark color in dark mode correctly", () => {
     const getCheckboxes = () =>
       cy.get(
-        "[data-testid=NewDefault] [data-testid=light-checkboxes] .chakra-checkbox__control"
+        "[data-testid=NewDefault] [data-testid=dark-checkboxes] .chakra-checkbox__control"
       );
+    cy.get("#switch-color-mode").click();
 
     // test the checked state
     const checked = getCheckboxes().eq(0);
@@ -239,122 +239,11 @@ describe("Bright Color", () => {
     cy.get("#reset-color-mode").realHover().click(); // Important! Need to .realHover() to remove the hover state from prior tests.
   });
 
-  it("displays dark color in light mode correctly", () => {
-    const getCheckboxes = () =>
-      cy.get(
-        "[data-testid=NewBrightColor] [data-testid=dark-checkboxes] .chakra-checkbox__control"
-      );
-
-    // test the checked state
-    const checked = getCheckboxes().eq(0);
-    checked
-      .should(
-        "have.css",
-        "background-color",
-        getRgbFromThemeColor("limeDark.9")
-      )
-      .should("have.css", "border-color", getRgbFromThemeColor("limeDark.9"))
-      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"))
-      .realHover()
-      .wait(200)
-      .should(
-        "have.css",
-        "background-color",
-        getRgbFromThemeColor("limeDark.10")
-      )
-      .should("have.css", "border-color", getRgbFromThemeColor("limeDark.10"));
-
-    // test the checked-disabled state
-    const checkedDisabled = getCheckboxes().eq(1);
-    checkedDisabled
-      .should(
-        "have.css",
-        "background-color",
-        getRgbFromThemeColor("_grayDark.4")
-      )
-      .should("have.css", "border-color", getRgbFromThemeColor("_grayDark.4"))
-      .should("have.css", "color", getRgbFromThemeColor("_grayDark.9"));
-
-    // test the indeterminate state
-    const indeterminate = getCheckboxes().eq(2);
-    indeterminate
-      .should(
-        "have.css",
-        "background-color",
-        getRgbFromThemeColor("limeDark.9")
-      )
-      .should("have.css", "border-color", getRgbFromThemeColor("limeDark.9"))
-      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
-
-    // test the disabled state
-    const disabled = getCheckboxes().eq(3);
-    disabled
-      .should(
-        "have.css",
-        "background-color",
-        getRgbFromThemeColor("_grayDark.3")
-      )
-      .should("have.css", "border-color", getRgbFromThemeColor("_grayDark.3"))
-      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
-
-    // test the invalid state
-    const invalid = getCheckboxes().eq(4);
-    invalid.should(
-      "have.css",
-      "border-color",
-      getRgbFromThemeColor("redDark.9")
-    );
-  });
-
   it("displays light color in light mode correctly", () => {
     const getCheckboxes = () =>
       cy.get(
         "[data-testid=NewBrightColor] [data-testid=light-checkboxes] .chakra-checkbox__control"
       );
-
-    // test the checked state
-    const checked = getCheckboxes().eq(0);
-    checked
-      .should("have.css", "background-color", getRgbFromThemeColor("lime.9"))
-      .should("have.css", "border-color", getRgbFromThemeColor("lime.9"))
-      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"))
-      .realHover()
-      .wait(200)
-      .should("have.css", "background-color", getRgbFromThemeColor("lime.10"))
-      .should("have.css", "border-color", getRgbFromThemeColor("lime.10"));
-
-    // test the checked-disabled state
-    const checkedDisabled = getCheckboxes().eq(1);
-    checkedDisabled
-      .should("have.css", "background-color", getRgbFromThemeColor("_gray.4"))
-      .should("have.css", "border-color", getRgbFromThemeColor("_gray.4"))
-      .should("have.css", "color", getRgbFromThemeColor("_grayDark.9"));
-
-    // test the indeterminate state
-    const indeterminate = getCheckboxes().eq(2);
-    indeterminate
-      .should("have.css", "background-color", getRgbFromThemeColor("lime.9"))
-      .should("have.css", "border-color", getRgbFromThemeColor("lime.9"))
-      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
-
-    // test the disabled state
-    const disabled = getCheckboxes().eq(3);
-    disabled
-      .should("have.css", "background-color", getRgbFromThemeColor("_gray.3"))
-      .should("have.css", "border-color", getRgbFromThemeColor("_gray.3"))
-      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
-
-    // test the invalid state
-    const invalid = getCheckboxes().eq(4);
-    invalid.should("have.css", "border-color", getRgbFromThemeColor("red.9"));
-  });
-
-  it("displays dark color in dark mode correctly", () => {
-    const getCheckboxes = () =>
-      cy.get(
-        "[data-testid=NewBrightColor] [data-testid=dark-checkboxes] .chakra-checkbox__control"
-      );
-    cy.get("#switch-color-mode").click();
 
     // test the checked state
     const checked = getCheckboxes().eq(0);
@@ -459,5 +348,116 @@ describe("Bright Color", () => {
       "border-color",
       getRgbFromThemeColor("redDark.9")
     );
+  });
+
+  it("displays dark color in light mode correctly", () => {
+    const getCheckboxes = () =>
+      cy.get(
+        "[data-testid=NewBrightColor] [data-testid=dark-checkboxes] .chakra-checkbox__control"
+      );
+
+    // test the checked state
+    const checked = getCheckboxes().eq(0);
+    checked
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("limeDark.9")
+      )
+      .should("have.css", "border-color", getRgbFromThemeColor("limeDark.9"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"))
+      .realHover()
+      .wait(200)
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("limeDark.10")
+      )
+      .should("have.css", "border-color", getRgbFromThemeColor("limeDark.10"));
+
+    // test the checked-disabled state
+    const checkedDisabled = getCheckboxes().eq(1);
+    checkedDisabled
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("_grayDark.4")
+      )
+      .should("have.css", "border-color", getRgbFromThemeColor("_grayDark.4"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.9"));
+
+    // test the indeterminate state
+    const indeterminate = getCheckboxes().eq(2);
+    indeterminate
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("limeDark.9")
+      )
+      .should("have.css", "border-color", getRgbFromThemeColor("limeDark.9"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
+
+    // test the disabled state
+    const disabled = getCheckboxes().eq(3);
+    disabled
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("_grayDark.3")
+      )
+      .should("have.css", "border-color", getRgbFromThemeColor("_grayDark.3"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
+
+    // test the invalid state
+    const invalid = getCheckboxes().eq(4);
+    invalid.should(
+      "have.css",
+      "border-color",
+      getRgbFromThemeColor("redDark.9")
+    );
+  });
+
+  it("displays dark color in dark mode correctly", () => {
+    const getCheckboxes = () =>
+      cy.get(
+        "[data-testid=NewBrightColor] [data-testid=dark-checkboxes] .chakra-checkbox__control"
+      );
+    cy.get("#switch-color-mode").click();
+
+    // test the checked state
+    const checked = getCheckboxes().eq(0);
+    checked
+      .should("have.css", "background-color", getRgbFromThemeColor("lime.9"))
+      .should("have.css", "border-color", getRgbFromThemeColor("lime.9"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"))
+      .realHover()
+      .wait(200)
+      .should("have.css", "background-color", getRgbFromThemeColor("lime.10"))
+      .should("have.css", "border-color", getRgbFromThemeColor("lime.10"));
+
+    // test the checked-disabled state
+    const checkedDisabled = getCheckboxes().eq(1);
+    checkedDisabled
+      .should("have.css", "background-color", getRgbFromThemeColor("_gray.4"))
+      .should("have.css", "border-color", getRgbFromThemeColor("_gray.4"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.9"));
+
+    // test the indeterminate state
+    const indeterminate = getCheckboxes().eq(2);
+    indeterminate
+      .should("have.css", "background-color", getRgbFromThemeColor("lime.9"))
+      .should("have.css", "border-color", getRgbFromThemeColor("lime.9"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
+
+    // test the disabled state
+    const disabled = getCheckboxes().eq(3);
+    disabled
+      .should("have.css", "background-color", getRgbFromThemeColor("_gray.3"))
+      .should("have.css", "border-color", getRgbFromThemeColor("_gray.3"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
+
+    // test the invalid state
+    const invalid = getCheckboxes().eq(4);
+    invalid.should("have.css", "border-color", getRgbFromThemeColor("red.9"));
   });
 });
