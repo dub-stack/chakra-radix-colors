1a1e5a4dd4de29be372464cfae18b6ccf24c93fc
Spencer Duball <spencer@spencerduball.com>
2022-04-28 21:00:23 -0500

    test(code): Added tests for the code component

---
+++ cypress/integration/components/code.spec.ts
@@ -0,0 +1,136 @@
+import { getRgbFromThemeColor } from "util/cypress-utils";
+
+describe("Default", () => {
+  beforeEach(() => {
+    cy.visit(
+      "http://localhost:6006/iframe.html?id=components-code--default&args=&viewMode=story"
+    );
+    cy.get("#reset-color-mode").click();
+  });
+
+  it("displays light color in light mode correctly", () => {
+    const getCodes = () =>
+      cy.get("[data-testid=NewDefault] [data-testid=light-code] .chakra-code");
+
+    // test the subtle
+    const subtleGray = getCodes().eq(0);
+    subtleGray
+      .should("have.css", "background-color", getRgbFromThemeColor("_gray.4"))
+      .should("have.css", "color", getRgbFromThemeColor("_gray.12"));
+
+    // test the solid
+    const solidBright = getCodes().eq(1);
+    solidBright
+      .should("have.css", "background-color", getRgbFromThemeColor("amber.9"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
+
+    // test the outline
+    const outlinePurple = getCodes().eq(2);
+    outlinePurple
+      .should(
+        "have.css",
+        "box-shadow",
+        `${getRgbFromThemeColor("purple.9")} 0px 0px 0px 1px inset`
+      )
+      .should("have.css", "color", getRgbFromThemeColor("purple.9"));
+  });
+
+  it("displays light color in dark mode correctly", () => {
+    const getCodes = () =>
+      cy.get("[data-testid=NewDefault] [data-testid=light-code] .chakra-code");
+    cy.get("#switch-color-mode").click();
+
+    // test the subtle
+    const subtleGray = getCodes().eq(0);
+    subtleGray
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("_grayDark.4")
+      )
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.12"));
+
+    // test the solid
+    const solidBright = getCodes().eq(1);
+    solidBright
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("amberDark.9")
+      )
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
+
+    // test the outline
+    const outlinePurple = getCodes().eq(2);
+    outlinePurple
+      .should(
+        "have.css",
+        "box-shadow",
+        `${getRgbFromThemeColor("purpleDark.9")} 0px 0px 0px 1px inset`
+      )
+      .should("have.css", "color", getRgbFromThemeColor("purpleDark.9"));
+  });
+
+  it("displays dark color in light mode correctly", () => {
+    const getCodes = () =>
+      cy.get("[data-testid=NewDefault] [data-testid=dark-code] .chakra-code");
+
+    // test the subtle
+    const subtleGray = getCodes().eq(0);
+    subtleGray
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("_grayDark.4")
+      )
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.12"));
+
+    // test the solid
+    const solidBright = getCodes().eq(1);
+    solidBright
+      .should(
+        "have.css",
+        "background-color",
+        getRgbFromThemeColor("amberDark.9")
+      )
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
+
+    // test the outline
+    const outlinePurple = getCodes().eq(2);
+    outlinePurple
+      .should(
+        "have.css",
+        "box-shadow",
+        `${getRgbFromThemeColor("purpleDark.9")} 0px 0px 0px 1px inset`
+      )
+      .should("have.css", "color", getRgbFromThemeColor("purpleDark.9"));
+  });
+
+  it("displays dark color in dark mode correctly", () => {
+    const getCodes = () =>
+      cy.get("[data-testid=NewDefault] [data-testid=dark-code] .chakra-code");
+    cy.get("#switch-color-mode").click();
+
+    // test the subtle
+    const subtleGray = getCodes().eq(0);
+    subtleGray
+      .should("have.css", "background-color", getRgbFromThemeColor("_gray.4"))
+      .should("have.css", "color", getRgbFromThemeColor("_gray.12"));
+
+    // test the solid
+    const solidBright = getCodes().eq(1);
+    solidBright
+      .should("have.css", "background-color", getRgbFromThemeColor("amber.9"))
+      .should("have.css", "color", getRgbFromThemeColor("_grayDark.1"));
+
+    // test the outline
+    const outlinePurple = getCodes().eq(2);
+    outlinePurple
+      .should(
+        "have.css",
+        "box-shadow",
+        `${getRgbFromThemeColor("purple.9")} 0px 0px 0px 1px inset`
+      )
+      .should("have.css", "color", getRgbFromThemeColor("purple.9"));
+  });
+});
--- src/theme/components/code/code.stories.tsx
+++ src/theme/components/code/code.stories.tsx
@@ -1,14 +1,87 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Code } from "@chakra-ui/react";
+import { Code, VStack, Text, Box, StackProps } from "@chakra-ui/react";
 import { Decorators } from "util/storybook-utils";
+import { useThemedColor } from "util/helpers";
 
 export default {
   title: "Components / Code",
   component: Code,
 };
 
-const _Basic = () => <Code colorScheme="pink">import React from react</Code>;
-export const Basic: ComponentStory<typeof _Basic> = (args) => {
-  return <Decorators newComponent={<_Basic />} oldComponent={<_Basic />} />;
+const NewDefault = (props: StackProps) => {
+  const c = useThemedColor();
+  return (
+    <VStack {...props}>
+      {/* display the light version in light mode */}
+      <Text as="h1" fontWeight="bold">
+        New Theme
+      </Text>
+      <Box bg={c("_gray.1")} p="4">
+        <VStack data-testid="light-code">
+          <Code>import React from "react"</Code>
+          <Code colorScheme="amber" variant="solid">
+            import React from "react"
+          </Code>
+          <Code colorScheme="purple" variant="outline">
+            import React from "react"
+          </Code>
+        </VStack>
+      </Box>
+      {/* display the light version in dark mode */}
+      <Box bg={c("_grayDark.1")} p="4">
+        <VStack data-testid="dark-code">
+          <Code colorScheme="_grayDark">import React from "react"</Code>
+          <Code colorScheme="amberDark" variant="solid">
+            import React from "react"
+          </Code>
+          <Code colorScheme="purpleDark" variant="outline">
+            import React from "react"
+          </Code>
+        </VStack>
+      </Box>
+    </VStack>
+  );
 };
+
+const OldDefault = (props: StackProps) => {
+  const c = useThemedColor();
+  return (
+    <VStack {...props}>
+      {/* display the light version in light mode */}
+      <Text as="h1" fontWeight="bold">
+        New Theme
+      </Text>
+      <Box p="4">
+        <VStack data-testid="light-code">
+          <Code>import React from "react"</Code>
+          <Code colorScheme="yellow" variant="solid">
+            import React from "react"
+          </Code>
+          <Code colorScheme="purple" variant="outline">
+            import React from "react"
+          </Code>
+        </VStack>
+      </Box>
+      {/* display the light version in dark mode */}
+      <Box p="4" visibility="hidden">
+        <VStack data-testid="dark-code">
+          <Code>import React from "react"</Code>
+          <Code colorScheme="yellow" variant="solid">
+            import React from "react"
+          </Code>
+          <Code colorScheme="purple" variant="outline">
+            import React from "react"
+          </Code>
+        </VStack>
+      </Box>
+    </VStack>
+  );
+};
+
+export const Default: ComponentStory<typeof VStack> = (args) => (
+  <Decorators
+    newComponent={<NewDefault data-testid="NewDefault" />}
+    oldComponent={<OldDefault data-testid="OldDefault" />}
+  />
+);
