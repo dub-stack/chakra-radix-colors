5e68274e54d1ec1e7f89f6342f03f99bae2f6f99
Spencer Duball <spencer@spencerduball.com>
2022-04-29 10:18:53 -0500

    test(heading): Added tests for the heading component

---
+++ src/theme/components/heading/heading.stories.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Text, Heading, StackProps, VStack, Box } from "@chakra-ui/react";
+import { Decorators } from "util/storybook-utils";
+import { useThemedColor } from "util/helpers";
+
+export default {
+  title: "Components / Heading",
+  component: Heading,
+} as ComponentMeta<typeof Heading>;
+
+const NewDefault = (props: StackProps) => {
+  const c = useThemedColor();
+  return (
+    <VStack {...props}>
+      <Text as="h1" fontWeight="bold">
+        New Theme
+      </Text>
+      <Box bg={c("_gray.1")} p="4">
+        <Heading>This is a heading.</Heading>
+      </Box>
+    </VStack>
+  );
+};
+
+const OldDefault = (props: StackProps) => {
+  return (
+    <VStack {...props}>
+      <Text as="h1" fontWeight="bold">
+        Old Theme
+      </Text>
+      <Box p="4">
+        <Heading>This is a heading.</Heading>
+      </Box>
+    </VStack>
+  );
+};
+
+export const Default: ComponentStory<typeof VStack> = (args) => (
+  <Decorators
+    newComponent={<NewDefault data-testid="NewDefault" />}
+    oldComponent={<OldDefault data-testid="OldDefault" />}
+  />
+);
